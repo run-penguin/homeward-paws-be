d92a4eba0ade86dcc5f8e89325bf851056aea739
runpenguin <run-penguin@naver.com>
2026-01-13 14:39:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/hp_be/HpBeApplication.java
@@ -0,0 +1,13 @@
+package com.hp_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hp_be/HpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
