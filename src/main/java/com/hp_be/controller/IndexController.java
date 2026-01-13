4996d816e5ad3eeaff8d6d27ffb2e774085036fb
runpenguin <run-penguin@naver.com>
2026-01-13 15:01:11 +0900

    docker 적용 테스트

---
+++ src/main/java/com/hp_be/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.hp_be.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "docker test";
+    }
+}
