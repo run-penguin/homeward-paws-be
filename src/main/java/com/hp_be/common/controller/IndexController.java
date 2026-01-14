38666d453dce760a5c2d18e830085a8b52f8bc6e
runpenguin <run-penguin@naver.com>
2026-01-14 14:31:21 +0900

    jpa 연동 테스트

--- src/main/java/com/hp_be/common/controller/IndexController.java
+++ src/main/java/com/hp_be/common/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.hp_be.controller;
+package com.hp_be.common.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/hp_be/common/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hp_be.common.repository;
+
+import com.hp_be.common.dao.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
